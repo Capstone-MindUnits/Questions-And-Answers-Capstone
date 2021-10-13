80ffa5102630e93d52def8a29cda2d6d0a144b0b
rimamenaa <rimamenaa98@gmail.com>
2021-10-13 23:47:59 +0100

    default answers done

--- qna/src/Components/Qna/Qna.js
+++ qna/src/Components/Qna/Qna.js
@@ -70,9 +70,9 @@ class Qna extends React.Component {
             <p>
               Helpful?
               <a href="#" className="underline ml-2 mr-1">
-                Yes 
+                Yes
               </a>
-               (25){" "}
+              (25){" "}
             </p>
             <p className=" ml-4">|</p>
             <p>
@@ -87,12 +87,12 @@ class Qna extends React.Component {
           <div className="w-2/3">
             <p className="font-sans font-bold text-base text-gray-600">
               Q: Whomst whatever whence wherefore?
-
             </p>
             <div className="answer mt-2">
               <p className="font-sans font-bold text-base text-gray-600">A:</p>
               <p className="font-sans font-ms ml-2 mt-1 text-gray-600 text-xs">
-               Tootsie roll pudding jelly-o jujubes jelly-o wafer biscuit danish ice cream. Cotton candy sweet roll candy.
+                Tootsie roll pudding jelly-o jujubes jelly-o wafer biscuit
+                danish ice cream. Cotton candy sweet roll candy.
               </p>
             </div>
 
@@ -114,6 +114,35 @@ class Qna extends React.Component {
                   Report
                 </a>
               </p>
+            </div>
+            <div>
+              <p className="font-sans font-ms ml-6 mt-2 text-gray-600 text-xs">
+                Yes, as you can see in these photos.
+              </p>
+              <div className="photos">
+                <div class="box-border mt-3 ml-6 h-12 w-20 p-4 border-2 border-gray-400	bg-gray-100	 "></div>
+                <div class="box-border mt-3 ml-3 h-12 w-20 p-4 border-2 border-gray-400	bg-gray-100	 "></div>
+                <div class="box-border mt-3 ml-3 h-12 w-20 p-4 border-2 border-gray-400	bg-gray-100	 "></div>
+              </div>
+              <div className="details mt-2">
+              <p className="font-sans font-ms ml-6 text-gray-400 ">
+                by User1337 - <b> Seller,</b> May 1, 2019
+              </p>
+              <p className=" ml-4 mr-4 text-gray-600">|</p>
+              <p className=" mr-4 text-gray-600">
+                Helpful?
+                <a href="#" className="underline ml-2 mr-1 ">
+                  Yes
+                </a>
+                (0){" "}
+              </p>
+              <p className=" mr-4 text-gray-600">|</p>
+              <p>
+                <a href="#" className="underline text-gray-600 ">
+                  Report
+                </a>
+              </p>
+            </div>
             </div>
           </div>
 
