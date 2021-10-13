45ad56cddc141866e451d74ed4134a58129e9c8c
rimamenaa <rimamenaa98@gmail.com>
2021-10-13 19:39:59 +0100

    search bar done

---
+++ qna/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Qna from './Components/Qna/Qna.js'
+import Search from './Components/Search/Search.js'
+
+
+function App() {
+  return (
+    <div className="app">
+     <Qna/>
+     <Search/>
+    </div>
+  );
+}
+
+export default App;
---
+++ qna/src/Components/Qna/Qna.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+class Qna extends React.Component {
+  render() {
+    return (
+      <div>
+        <p className="font-sans font-ms text-sm text-gray-600">QUESTIONS & ANSWERS</p>
+        <br/>
+        <div>
+          <form className="relative">
+            <svg
+              width="20"
+              height="20"
+              fill="currentColor"
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-800 "
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+              />
+            </svg>
+            <input
+              className="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-medium text-black placeholder-gray-600 border  border-gray-600 rounded-none py-2 pl-4 font-bold text-xs h-12	"
+              type="text"
+              aria-label="Filter projects"
+              placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..."
+            />
+          </form>
+        </div>
+        <br/>
+        <div className="question">
+        <div className="w-2/3">
+        <p className="font-sans font-bold text-base text-gray-600">Q: Who what what which when where why whether how?</p>         
+        <br/>
+        <div className="answer">  
+        <p className="font-sans font-bold text-base text-gray-600">A:</p>
+        <p className="font-sans font-ms ml-2 text-gray-600 text-xs"> 
+        Icing macaron bear claw jelly beans chocolate cake. Cookie oat cake chocolate halvah jelly cake cotton candy soufflé topping. Jujubes topping cake gummies lemon drops.</p>
+          </div>
+          <br/>
+
+          <div className="details">  
+        <p className="font-sans font-ms ml-6 text-gray-400 text-xs"> by User1234, January 1, 2019</p>
+        <p className="text-xs ml-6 mr-6">|</p>
+        <p className="text-xs mr-6">Helpful? <a  href="#" className="underline ">Yes </a>(2)</p>
+        <p className="text-xs mr-6">|</p>
+        <p className="text-xs"><a  href="#" className="underline ">Report</a></p>
+
+        </div>
+      
+        </div>
+
+        <p className="text-xs ">Helpful? <a  href="#" className="underline ">Yes </a>(25)</p>
+        <p className="text-xs">|</p>
+        <a  href="#" className="underline text-xs">Add Answer </a>
+        </div>
+
+
+
+      </div>
+    );
+  }
+}
+
+export default Qna;
