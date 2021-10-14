818d425902776cdb30839d9b7ce72891265ce801
rimamenaa <rimamenaa98@gmail.com>
2021-10-14 14:20:08 +0100

    one button in progress

--- qna/src/Components/Buttons/Buttons.js
+++ qna/src/Components/Buttons/Buttons.js
@@ -4,8 +4,8 @@ class Buttons extends React.Component {
     render() {
         return (
             <div>
-                <button className="w-1/3 h-16 border m-2 border-black font-bold " >
-                <p>MORE ANSWERED QUESTIONS</p>
+                <button className="w-1/3 h-12 border border-gray-600 " >
+                <p className="font-sans font-bold text-sm text-gray-600">MORE ANSWERED QUESTIONS</p>
                 </button>
             </div>
         )
