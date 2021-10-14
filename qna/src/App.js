bb7c30aa56c3e3ce26f05f4404fff7a4a96695ed
rimamenaa <rimamenaa98@gmail.com>
2021-10-14 10:50:52 +0100

    made it responsive using the grid

--- qna/src/App.js
+++ qna/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-
 import Qna from './Components/Qna/Qna.js'
 import Search from './Components/Search/Search.js'
 
 
 function App() {
   return (
-    <div className="app">
-     <Qna/>
-     <Search/>
+    <div className="app">  
+      <div className="grid grid-rows-2 row-span-2 h-48 grid-flow-col w-3/5  mx-auto">
+         <Search />
+         <Qna/>
+      </div>  
     </div>
   );
 }
--- qna/src/Components/Search/Search.js
+++ qna/src/Components/Search/Search.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React from "react";
 
 class Search extends React.Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div>
+          <p className="font-sans font-ms text-sm text-gray-600">
+            QUESTIONS & ANSWERS
+          </p>
+        </div>
+        <form className="relative">
+          <svg
+            width="20"
+            height="20"
+            fill="currentColor"
+            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-800 "
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+            />
+          </svg>
+          <input
+            className="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-medium text-black placeholder-gray-600 border  border-gray-600 rounded-none py-2 pl-4 font-bold text-xs h-12	"
+            type="text"
+            aria-label="Filter projects"
+            placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..."
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Search;
